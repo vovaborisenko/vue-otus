e733ce7cfc84c56f38442f6dc285b536b503574e
Uladzimir Barysenka <vovaborisenko@live.com>
2022-04-24 18:47:36 +0300

    adds more initial settings

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,11 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: '@import "~@/assets/styles/variables"; @import "~@/assets/styles/mixins";',
+      },
+    },
+  },
 });
